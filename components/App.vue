3307ada4f9ac7bb2762220eb4259ee01008d212c
kamil.smyk <kamil.smyk00@gmail.com>
2023-02-10 14:24:50 +0100

    Initial commit

---
+++ components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class='app'>
+    <Navbar/>
+    <div class='routes-container'>
+      <nuxt/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar.vue'
+
+export default {
+  components: { Navbar }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ router.js
@@ -0,0 +1,40 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import App from '@/components/App.vue'
+
+import dashboard from '@/pages/dashboard.vue'
+
+Vue.use(Router)
+
+export function createRouter() {
+  return new Router({
+    mode: 'history',
+    linkActiveClass: 'nav-active',
+    scrollBehavior(to) {
+      if (to.hash) {
+        return {
+          selector: to.hash,
+          behavior: 'smooth'
+        };
+      }
+    },
+    routes: [
+      {
+        path: '',
+        component: App,
+        children: [
+          {
+            path: '',
+            component: dashboard,
+            name: 'dashboard'
+          }
+        ]
+      }
+    ]
+  })
+}
+
+
+
+
+
