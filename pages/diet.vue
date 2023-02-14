572f037f19db416c829b16ba6a79681de3d48b00
kamil.smyk <kamil.smyk00@gmail.com>
2023-02-14 11:40:01 +0100

    added pie chart

--- pages/diet.vue
+++ pages/diet.vue
@@ -1,18 +1,32 @@
 <template>
   <div class='diet-container'>
     <div class='title'>
-      <span>Twój dzień</span>
-      <button class='add-btn'>+</button>
+      <span>Twój dzień</span><br>
+      <div class='meal-add'>
+        <button class='meal-btn'>Dodaj posiłek</button>
+      </div>
     </div>
     <div class='card-container'>
       <div class='diet-card'>
-        <div class='diet-title'>
+        <div class='meal-title'>
           <h2>ŚNIADANIE</h2>
+          <div class='meal-add'>
+            <button class='add-btn'>+</button>
+          </div>
         </div>
         <div class='diet-content'>
-          <span>TEXT TEXT TEXT</span>
+            <div class='item-title'>
+              <span></span>
+            </div>
+            <div class='item-content'>
+              <span class='item-makro'>Kcal: </span>
+              <span class='item-makro'>W: </span>
+              <span class='item-makro'>B: </span>
+              <span class='item-makro'>T: </span>
+            </div>
         </div>
       </div>
+
     </div>
     <div class='diet-progress'>
       progres
@@ -22,7 +36,11 @@
 
 <script>
 export default {
-  name: 'diet'
+  name: 'diet',
+  data() {
+    return {
+    }
+  }
 }
 </script>
 
