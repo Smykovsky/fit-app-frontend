d478e37c6279f743e152053fff75412575b22d39
Smyku <kamil.smyk00@gmail.com>
2023-06-18 15:03:09 +0200

    add recipeById page and style

--- pages/recipeById.vue
+++ pages/recipeById.vue
@@ -1,6 +1,40 @@
 <template>
-  <div>
-    <h1>{{ id }}</h1>
+  <div class='recipe-container'>
+    <div class='recipe-card'>
+      <div class='recipe-img'>
+        <img v-if="recipe && recipe.photoUrl" :src='recipe.photoUrl' alt="Opis obrazka">
+      </div>
+      <div class='recipe-data'>
+        <div class='recipe-header'>
+          <span v-if='recipe && recipe.name'>{{ recipe.name }}</span>
+        </div>
+        <div class='recipe-info'>
+          <div class='recipe-menu'>
+            <nav>
+              <ul>
+                <li :class="{'active': isActiveTab === 'tab1'}" @click="menuDescription" >Opis przepisu</li>
+                <li :class="{'active': isActiveTab === 'tab2'}" @click="menuInstruction" >Instrukcja</li>
+                <li :class="{'active': isActiveTab === 'tab3'}" @click="menuMacro" >Makro składniki</li>
+              </ul>
+            </nav>
+            <div v-if='description'  class='menu-description'>
+              <span v-if='recipe'>{{ recipe.description }}</span>
+            </div>
+            <div v-if='instruction' class='menu-instruction'>
+              <span v-if='recipe'>{{ recipe.instruction }}</span>
+            </div>
+            <div v-if='macro' class='menu-macro'>
+              <ul>
+                <li>Kalorie:  {{ recipe.kcal }}</li>
+                <li>Węglowodany: {{ recipe.carbohydrates }}</li>
+                <li>Białko: {{ recipe.protein }}</li>
+                <li>Tłuszcz: {{ recipe.fat }}</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,7 +49,11 @@ export default {
   },
   data() {
     return {
+      isActiveTab: 'tab1',
       recipe: null,
+      description: true,
+      instruction: false,
+      macro: false,
     }
   },
   created() {
@@ -31,11 +69,31 @@ export default {
       }).catch(error => {
         console.log(error)
       })
+    },
+
+    menuDescription() {
+      this.instruction = false
+      this.macro = false
+      this.description = true
+      this.isActiveTab = "tab1"
+
+    },
+    menuInstruction() {
+      this.description = false
+      this.macro = false
+      this.instruction = true
+      this.isActiveTab = "tab2"
+    },
+    menuMacro() {
+      this.description = false
+      this.instruction = false
+      this.macro = true
+      this.isActiveTab = "tab3"
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style lang="sass">
+@import "assets/recipeById.sass"
 </style>
