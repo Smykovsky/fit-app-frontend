76c31e0b863d63595389d54df577da6db3b39a61
Smyku <kamil.smyk00@gmail.com>
2024-11-27 21:19:10 +0100

    p

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class='nav-container' v-bind:class="{active: $store.getters['store/getMenuIsActive']}">
+  <div class='nav-container' v-bind:class="{ active: $store.getters['store/getMenuIsActive'] }">
     <nav>
       <ul>
         <li class='logo'>
@@ -9,30 +9,42 @@
 
           <span>{{username}}</span>
         </li>
-        <li class='dashboard-li'><router-link class="text-decoration-none" :to="{name: 'dashboard'}">
-          <font-awesome-icon class='icon' icon="fa-solid fa-gauge" />
-          <span class='nav-item'>Dashboard</span>
-        </router-link></li>
-        <li class='dashboard-li'><router-link class="text-decoration-none" :to="{name: 'diet'}">
-          <font-awesome-icon class='icon' icon="fa-solid fa-utensils" />
-          <span class='nav-item'>Moje posiłki</span>
-        </router-link></li>
-        <li class='dashboard-li'><router-link class="text-decoration-none" :to="{name: 'shoppingList'}">
-          <font-awesome-icon class='icon' icon="fa-solid fa-list-check" />
-          <span class='nav-item'>Lista zakupów</span>
-        </router-link></li>
-        <li class='dashboard-li'><router-link class="text-decoration-none" :to="{name: 'progress'}">
-          <font-awesome-icon class='icon' icon="fa-solid fa-weight-scale" />
-          <span class='nav-item'>Mój progres</span>
-        </router-link></li>
-        <li class='dashboard-li'><router-link class="text-decoration-none" :to="{name: 'recipes'}">
-          <font-awesome-icon class='icon' icon="fa-solid fa-utensils" />
-          <span class='nav-item'>Przepisy</span>
-        </router-link></li>
-        <li class='dashboard-li'><router-link class="text-decoration-none" :to="{name: 'contact'}">
-          <font-awesome-icon class='icon' icon="fa-solid fa-phone" />
-          <span class='nav-item'>Kontakt</span>
-        </router-link></li>
+        <li class='dashboard-li'>
+          <router-link class="text-decoration-none" :to="{name: 'dashboard'}">
+            <font-awesome-icon class='icon' icon="fa-solid fa-gauge" />
+            <span class='nav-item'>Dashboard</span>
+          </router-link>
+        </li>
+        <li class='dashboard-li'>
+          <router-link class="text-decoration-none" :to="{name: 'diet'}">
+            <font-awesome-icon class='icon' icon="fa-solid fa-utensils" />
+            <span class='nav-item'>Moje posiłki</span>
+          </router-link>
+        </li>
+        <li class='dashboard-li'>
+          <router-link class="text-decoration-none" :to="{name: 'shoppingList'}">
+            <font-awesome-icon class='icon' icon="fa-solid fa-list-check" />
+            <span class='nav-item'>Lista zakupów</span>
+          </router-link>
+        </li>
+        <li class='dashboard-li'>
+          <router-link class="text-decoration-none" :to="{name: 'progress'}">
+            <font-awesome-icon class='icon' icon="fa-solid fa-weight-scale" />
+            <span class='nav-item'>Mój progres</span>
+          </router-link>
+        </li>
+        <li class='dashboard-li'>
+          <router-link class="text-decoration-none" :to="{name: 'recipes'}">
+            <font-awesome-icon class='icon' icon="fa-solid fa-utensils" />
+            <span class='nav-item'>Przepisy</span>
+          </router-link>
+        </li>
+        <li class='dashboard-li'>
+          <router-link class="text-decoration-none" :to="{name: 'contact'}">
+            <font-awesome-icon class='icon' icon="fa-solid fa-phone" />
+            <span class='nav-item'>Kontakt</span>
+          </router-link>
+        </li>
         <li class='dashboard-li' v-b-modal.modal-user><a class="text-decoration-none">
           <font-awesome-icon class='icon' icon="fa-solid fa-user" />
           <span v-if='isPersonalized == false' class='nav-item'>Personalizuj profil</span>
@@ -42,24 +54,26 @@
           <font-awesome-icon class='icon' icon="fa-solid fa-key" />
           <span class='nav-item'>Zmień hasło</span>
         </a></li>
-        <li @click='logout'  class='dashboard-li'><a class="text-decoration-none">
-          <font-awesome-icon style='color: white' class='icon' icon="fa-solid fa-right-from-bracket" />
-          <span class='nav-item'>Wyloguj się</span>
-        </a></li>
-        <li v-if='isAdmin' class='dashboard-li admin'><router-link class="text-decoration-none" :to="{name: 'admin'}">
-          <font-awesome-icon class='icon' icon="fa-solid fa-user" />
-          <span class='nav-item'>Panel Administracyjny</span>
-        </router-link></li>
+        <li @click='logout' class='dashboard-li'>
+          <a class="text-decoration-none">
+            <font-awesome-icon style='color: white' class='icon' icon="fa-solid fa-right-from-bracket" />
+            <span class='nav-item'>Wyloguj się</span>
+          </a>
+        </li>
+        <li v-if='isAdmin' class='dashboard-li admin'>
+          <router-link class="text-decoration-none" :to="{name: 'admin'}">
+            <font-awesome-icon class='icon' icon="fa-solid fa-user" />
+            <span class='nav-item'>Panel Administracyjny</span>
+          </router-link>
+        </li>
       </ul>
     </nav>
     <ModalsModalPersonalize/>
     <ModalsModalChangePassword/>
   </div>
-
 </template>
 
 <script>
-
 export default {
   name: 'Navbar',
   data() {
@@ -73,7 +87,7 @@ export default {
   },
   mounted() {
     this.checkAdminStatus(),
-    this.isUserPersonalized();
+      this.isUserPersonalized();
     this.loadUsername()
   },
   created() {
@@ -92,9 +106,7 @@ export default {
     },
     checkAdminStatus() {
       const bool = this.$auth.user.roles.includes("admin");
-      console.log(this.$auth.user.roles.includes("admin"))
       this.isAdmin = bool
-      console.log(bool)
     },
     isUserPersonalized() {
       if (this.$auth.user.height == null || this.$auth.user.weight == null || this.$auth.user.age == null || this.$auth.user.gender == null) {
